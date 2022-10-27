b4b7634fa38a139bb6d14d405966d338f04c8680
miicristina <107210330+miicristina@users.noreply.github.com>
2022-10-27 17:38:01 -0300

    Add files via upload

---
+++ Atividade 03/script.js
@@ -0,0 +1,23 @@
+var signo = prompt ('Digite seu signo: ')
+
+switch (signo) {
+    case signo = 'Áries' :
+        console.log( 'Áries - de 21 de março a 20 de abril' ) ;
+        break;
+    case signo = 'Touro' :
+        console.log( 'Touro -  de 21 abril a 20 maio' ) ;
+        break;
+    case signo = 'Gêmeos' :
+        console.log( 'Gêmeos -  de 21 maio a 20 junho' ) ;
+        break;
+    case signo = 'Câncer' :
+        console.log( 'Câncer -  de 21 junho a 22 julho' ) ;
+        break;
+    case signo = 'Leão' :
+        console.log( 'Leão - de 23 julho a 22 agosto' ) ;
+        break;
+    case signo = 'Virgem' :
+        console.log( 'Virgem - de 23 agosto a 22 setembro' ) ;
+        break;
+    
+}
